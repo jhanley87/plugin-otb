7d31402533cbe3a0c55d389fca41837ef11e1aca
Jordan Hanley <jhanley@twilio.com>
2022-06-28 10:34:30 +0100

    params

--- src/OnthebeachPlugin.js
+++ src/OnthebeachPlugin.js
@@ -18,13 +18,13 @@ export default class OnthebeachPlugin extends FlexPlugin {
    */
   async init(flex, manager) {
     const options = { sortOrder: -1 };
-    flex.AgentDesktopView.Panel1.Content.add(<CustomTaskList key="OnthebeachPlugin-component" />, options);
+    //flex.AgentDesktopView.Panel1.Content.add(<CustomTaskList key="OnthebeachPlugin-component" />, options);
 
 
 
     flex.CRMContainer.defaultProps.uriCallback = (task) =>
       task
-        ? `https://www.bing.com/search?q=${task.attributes.name}`
+        ? `https://www.bing.com/search?q=${task.attributes.myb}`
         : "http://bing.com/";
 
     flex.MainHeader.defaultProps.logoUrl =
